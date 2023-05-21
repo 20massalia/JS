cfc86ac50580c5830d02ffc2c0a24f8b850bdd1b
Jean <20massalia@kakao.com>
2023-05-21 19:48:12 +0900

    초기화 수정

--- 계산기/calculator.js
+++ 계산기/calculator.js
@@ -1,14 +1,29 @@
+var isCalculatedFormula = false;
+
 function show(char) {
-    var formula = document.getElementById('result');
-    formula.value += char;
+    if (!isCalculatedFormula) {
+        var formula = document.getElementById('result');
+        formula.value += char;
+    }
+
+    else {
+        document.getElementById('result').value = "";
+        var formula = document.getElementById('result');
+        formula.value += char;
+        isCalculatedFormula = false;
+    }
 }
 
 function calculate() {
     var formula = document.getElementById('result');
     var result = eval(formula.value);
     document.getElementById('result').value = result;
+    isCalculatedFormula = true;
+    //alert("isCalculatedFormula is " + isCalculatedFormula);
 }
 
 function ac() {
     document.getElementById('result').value = "";
+    isCalculatedFormula = false;
+    //alert("isCalculatedFormula is " + isCalculatedFormula);
 }
